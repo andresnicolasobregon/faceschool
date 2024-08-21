7d5fdb88eb56a1b344efa87ebb8320adacbb3222
andresobregon <andresnicolasobregon@gmail.com>
2024-08-21 08:31:39 -0300

    registrar

--- funciones.js
+++ funciones.js
@@ -5,3 +5,10 @@ function like() {
     document.getElementById("megusta").innerHTML = a + " me gusta";
     document.getElementById("corazon").style.color="red";
 }
+
+
+function registrar() {
+    document.getElementById("boxconfircontrasena").style.display = "flex";
+    document.getElementById("btnlogin").style.display = "none";
+    document.getElementById("btnregistrar").innerHTML = "Crear nuevo usuario"
+}
